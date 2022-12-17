799cb519d081bbd57304195555ac9445b8d3772f
Casey <cmurphy20@student.cvtc.edu>
2022-12-16 21:21:49 -0600

    skeleton for title animation

---
+++ js/titleFadeIn.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+  console.log("confirm");
+  
+  let $title = $('.introTitle');
+  let $content = $('.introP');
+
+  $title.css({
+    display: "hidden",
+    bottom: "35px"
+  });
+
+  $content.css({
+    display: "hidden",
+    bottom: "35px"
+  });
+
+  $('document').ready(function() {
+
+  })
+})
+
+  
+
+  
